acf0dcfbb0d4e2a37e5848e84f978b89bcddee58
春晖 <chunhui.wang@yintech.cn>
2021-11-15 14:34:05 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDom from 'react-dom'
+import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const dom = React.createElement('div', { id: 'app' }, 'HelloWorld')
+// root元素存在于public中的index.html中
+ReactDom.render(dom, document.getElementById('root'))
